5507e953188803185f987914698ce3f419fc9617
Harry Lachenmayer <harrylachenmayer@gmail.com>
2017-10-13 16:09:14 +0100

    add scroll nagger

--- app/initialize.js
+++ app/initialize.js
@@ -3,4 +3,10 @@ const Letters = require('./letters')
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('hackcampus-letters')
   const letters = new Letters(container, 'HACKCAMPUS', 'black')
+
+  const scrollArrow = $('.scroll-arrow')
+  scrollArrow.addClass('show')
+  $(window).one('scroll', () => {
+    scrollArrow.addClass('fade-out')
+  })
 });
